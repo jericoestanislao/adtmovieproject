58a770986585e4eb4919e342616ddb6b30de3b36
jericoestanislao <jericoestanislao@hotmail.com>
2024-12-06 21:05:14 +0800

    Add files via upload

--- ADT313_MovieProject_Admin/src/pages/Public/Register/Register.jsx
+++ ADT313_MovieProject_Admin/src/pages/Public/Register/Register.jsx
@@ -1,7 +1,7 @@
 import { useState, useRef, useCallback, useEffect } from 'react';
+import './Register.css';
 import { useNavigate } from 'react-router-dom';
 import { useDebounce } from '../../../utils/hooks/useDebounce';
-import './Register.css';
 import axios from 'axios';
 
 function Register() {
@@ -10,7 +10,7 @@ function Register() {
   const [firstName, setFirstName] = useState('');
   const [middleName, setMiddleName] = useState('');
   const [lastName, setLastName] = useState('');
-  const [contactNo, setContactNo] = useState('');
+  const [contactNo, setcontactNo] = useState('');
   const [role, setRole] = useState('');
   const [isFieldsDirty, setIsFieldsDirty] = useState(false);
   const emailRef = useRef();
@@ -21,18 +21,20 @@ function Register() {
   const contactNoRef = useRef();
   const roleRef = useRef();
   const [isShowPassword, setIsShowPassword] = useState(false);
-  const userInputDebounce = useDebounce(
-    { email, password, firstName, middleName, lastName, contactNo, role },
-    2000
-  );
+  const userInputDebounce = useDebounce({ email, password, firstName, middleName, lastName, contactNo, role }, 2000);
   const [debounceState, setDebounceState] = useState(false);
   const [status, setStatus] = useState('idle');
+
+
+  const [alertMessage, setAlertMessage] = useState('');
+  const [isError, setIsError] = useState(false);
+
 
   const navigate = useNavigate();
 
   const handleShowPassword = useCallback(() => {
     setIsShowPassword((value) => !value);
-  }, [isShowPassword]);
+  }, []);
 
   const handleOnChange = (event, type) => {
     setDebounceState(false);
@@ -41,12 +43,13 @@ function Register() {
     switch (type) {
       case 'email':
         setEmail(event.target.value);
+
         break;
 
       case 'password':
         setPassword(event.target.value);
         break;
-
+      
       case 'firstName':
         setFirstName(event.target.value);
         break;
@@ -59,8 +62,9 @@ function Register() {
         setLastName(event.target.value);
         break;
 
+
       case 'contactNo':
-        setContactNo(event.target.value);
+        setcontactNo(event.target.value);
         break;
 
       case 'role':
@@ -72,6 +76,14 @@ function Register() {
     }
   };
 
+  let apiEndpoint;
+
+  if (window.location.pathname.includes('/admin')) {
+    apiEndpoint = '/admin/register';
+  } else {
+    apiEndpoint = '/user/register';
+  }
+
   const handleRegister = async () => {
     const data = { email, password, firstName, middleName, lastName, contactNo, role };
     setStatus('loading');
@@ -79,27 +91,33 @@ function Register() {
 
     await axios({
       method: 'post',
-      url: '/user/register',
+      url: apiEndpoint,
       data,
       headers: { 'Access-Control-Allow-Origin': '*' },
     })
       .then((res) => {
         console.log(res);
         localStorage.setItem('accessToken', res.data.access_token);
-        navigate('/');
-        setStatus('idle');
+
+        //show the alert message
+        setIsError(false);
+        setAlertMessage(res.data.message);
+        setTimeout(() => {
+          navigate('/');
+          setStatus('idle');
+        }, 3000);
       })
       .catch((e) => {
         console.log(e);
         setStatus('idle');
-        alert(e.response.data.message);
-      });
-  };
 
-  const handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      handleRegister();
-    }
+        //show the alert message
+        setIsError(true);
+        setAlertMessage(e.response?.data?.message || e.message);
+        setTimeout(() => setAlertMessage(''), 3000);
+
+        //alert(e.response.data.message);
+      });
   };
 
   useEffect(() => {
@@ -107,170 +125,110 @@ function Register() {
   }, [userInputDebounce]);
 
   return (
-    <div className="Register">
-      <div className="main-container">
-        <h3 className="h3-register">Register</h3>
+    <div className='Register'>
+      <div className='main-container'>
+        {alertMessage && (<div className={`alert-box ${isError ? 'error' : 'success'}`}>{alertMessage}</div>)}
         <form>
-          <div className="form-container">
-            <div>
-              <div className="form-group">
-                <label style={{ color: 'rgb(196, 196, 196)' }}>First Name:</label>
-                <input
-                  type="text"
-                  name="firstName"
-                  ref={firstNameRef}
-                  style={{
-                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                    color: '#fff',
-                    border: 'none',
-                    borderRadius: '4px',
-                    outline: '1px solid rgb(196, 196, 196)',
-                  }}
-                  onChange={(e) => handleOnChange(e, 'firstName')}
-                  onKeyDown={handleKeyPress}
-                />
-              </div>
+          <div className='form-container'>
+            <div className='register-header'>
+            <h1>Register</h1>
             </div>
             <div>
-              <div className="form-group">
-                <label style={{ color: 'rgb(196, 196, 196)' }}>Middle Name:</label>
-                <input
-                  type="text"
-                  name="middleName"
-                  ref={middleNameRef}
-                  style={{
-                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                    color: '#fff',
-                    border: 'none',
-                    borderRadius: '4px',
-                    outline: '1px solid rgb(196, 196, 196)',
-                  }}
-                  onChange={(e) => handleOnChange(e, 'middleName')}
-                  onKeyDown={handleKeyPress}
-                />
-              </div>
-            </div>
-            <div>
-              <div className="form-group">
-                <label style={{ color: 'rgb(196, 196, 196)' }}>Last Name:</label>
-                <input
-                  type="text"
-                  name="lastName"
-                  ref={lastNameRef}
-                  style={{
-                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                    color: '#fff',
-                    border: 'none',
-                    borderRadius: '4px',
-                    outline: '1px solid rgb(196, 196, 196)',
-                  }}
-                  onChange={(e) => handleOnChange(e, 'lastName')}
-                  onKeyDown={handleKeyPress}
-                />
-              </div>
-            </div>
-            <div>
-              <div className="form-group">
-                <label style={{ color: 'rgb(196, 196, 196)' }}>Contact Number:</label>
-                <input
-                  type="text"
-                  name="contactNo"
-                  ref={contactNoRef}
-                  style={{
-                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                    color: '#fff',
-                    border: 'none',
-                    borderRadius: '4px',
-                    outline: '1px solid rgb(196, 196, 196)',
-                  }}
-                  onChange={(e) => handleOnChange(e, 'contactNo')}
-                  onKeyDown={handleKeyPress}
-                />
-              </div>
-            </div>
-            <div>
-              <div className="form-group">
-                <label style={{ color: 'rgb(196, 196, 196)' }}>Role:</label>
-                <input
-                  type="text"
-                  name="role"
-                  ref={roleRef}
-                  style={{
-                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                    color: '#fff',
-                    border: 'none',
-                    borderRadius: '4px',
-                    outline: '1px solid rgb(196, 196, 196)',
-                  }}
-                  onChange={(e) => handleOnChange(e, 'role')}
-                  onKeyDown={handleKeyPress}
-                />
-              </div>
-            </div>
-            <div>
-              <div className="form-group">
-                <label style={{ color: 'rgb(196, 196, 196)' }}>E-mail:</label>
-                <input
-                  type="text"
-                  name="email"
-                  ref={emailRef}
-                  style={{
-                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                    color: '#fff',
-                    border: 'none',
-                    borderRadius: '4px',
-                    outline: '1px solid rgb(196, 196, 196)',
-                  }}
-                  onChange={(e) => handleOnChange(e, 'email')}
-                  onKeyDown={handleKeyPress}
-                />
-              </div>
-            </div>
-            <div>
-              <div className="form-group">
-                <label style={{ color: 'rgb(196, 196, 196)' }}>Password:</label>
-                <input
-                  type={isShowPassword ? 'text' : 'password'}
-                  name="password"
-                  ref={passwordRef}
-                  style={{
-                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                    color: '#fff',
-                    border: 'none',
-                    borderRadius: '4px',
-                    outline: '1px solid rgb(196, 196, 196)',
-                  }}
-                  onChange={(e) => handleOnChange(e, 'password')}
-                  onKeyDown={handleKeyPress}
-                />
-              </div>
-            </div>
-            <div
-              className="show-password"
-              onClick={handleShowPassword}
-              style={{ cursor: 'pointer', display: 'inline-block' }}
-            >
-              <img
-                src={isShowPassword ? '/hide-pass.png' : '/show-pass.png'}
-                alt={isShowPassword ? 'Hide Password' : 'Show Password'}
-              />
-            </div>
 
-            <div className="submit-container">
-              <button
-                type="button"
-                disabled={status === 'loading'}
-                onClick={handleRegister}
-              >
-                {status === 'idle' ? 'Register' : 'Loading'}
-              </button>
-            </div>
-            <div className="register-container">
-              <span>
-                <small style={{ color: 'lightgray' }}>
-                  Already have an account? <a href="/">Login</a>
-                </small>
-              </span>
+              <div>
+                <div className='form-group'>
+                  <label>First Name:</label>
+                  <input type='text' name='firstName' ref={firstNameRef} onChange={(e) => handleOnChange(e, 'firstName')} required/>
+                </div>
+                {debounceState && isFieldsDirty && firstName === '' && (
+                  <span className='errors'>This field is required</span>
+                )}
+              </div>
+
+              <div>
+                <div className='form-group'>
+                  <label>Middle Name:</label>
+                  <input type='text' name='middleName' ref={middleNameRef} onChange={(e) => handleOnChange(e, 'middleName')} required/>
+                </div>
+                {debounceState && isFieldsDirty && middleName === '' && (
+                  <span className='errors'>This field is required</span>
+                )}
+              </div>
+
+              <div>
+                <div className='form-group'>
+                  <label>Last Name:</label>
+                  <input type='text' name='lastName' ref={lastNameRef} onChange={(e) => handleOnChange(e, 'lastName')} required/>
+                </div>
+                {debounceState && isFieldsDirty && lastName === '' && (
+                  <span className='errors'>This field is required</span>
+                )}
+              </div>
+
+                <div>
+                  <div className='form-group'>
+                    <label>Contact Number:</label>
+                    <input type='text' name='contactNo' ref={contactNoRef} onChange={(e) => handleOnChange(e, 'contactNo')} required/>
+                  </div>
+                  {debounceState && isFieldsDirty && contactNo === '' && (
+                    <span className='errors'>This field is required</span>
+                  )}
+                </div>
+
+              <div>
+                <div className='form-group'>
+                  <label>Email:</label>
+                  <input type='text' name='email' ref={emailRef} onChange={(e) => handleOnChange(e, 'email')} required/>
+                  </div>
+                  {debounceState && isFieldsDirty && email === '' && (
+                    <span className='errors'>This field is required</span>
+                  )}
+                </div>
+                <div>
+                  <div className='form-group'>
+                    <label>Password:</label>
+                    <div>
+                      <input type={isShowPassword ? 'text' : 'password'} name='password' ref={passwordRef} onChange={(e) => handleOnChange(e, 'password')} />
+                      <span className={`fas ${isShowPassword ? '/public/show-pass.png' : '/public/hide-pass.png'}` } id='icon-toggle' onClick={handleShowPassword} />
+                    </div>
+                  </div>
+                  {debounceState && isFieldsDirty && password === '' && (
+                    <span className='errors'>This field is required</span>
+                  )}
+                </div>
+              </div>
+
+              <div className='submit-container'>
+                <button
+                  type='button'
+                  className='register-btn'
+                  disabled={status === 'loading'}
+                  onClick={() => {
+                    if (status === 'loading') {
+                      return;
+                    }
+                    if (email && password && firstName && middleName && contactNo) {
+                      handleRegister({
+                        type: 'register',
+                        user: { email, password, firstName,middleName,lastName,contactNo,role },
+                      });
+                    } else {
+                      setIsFieldsDirty(true);
+                      if (email === '') {
+                        emailRef.current.focus();
+                      }
+
+                      if (password === '') {
+                        passwordRef.current.focus();
+                      }
+                    }
+                  }}
+                >
+                  {status === 'idle' ? 'Register' : 'Loading'}
+                </button>
+              </div>
+              <div className='register-container'>
+                <span><small>Already have an account? <a href='/'>Login</a></small></span>
             </div>
           </div>
         </form>
@@ -278,5 +236,9 @@ function Register() {
     </div>
   );
 }
+
+
+
+
 
 export default Register;
