235e66d7df5fb3904f756be41a667c35e9b00fcb
jericoestanislao <jericoestanislao@hotmail.com>
2024-12-06 22:20:50 +0800

    updated code & ui design

--- ADT313_MovieProject_Admin/src/pages/Main/Movie/Movie.jsx
+++ ADT313_MovieProject_Admin/src/pages/Main/Movie/Movie.jsx
@@ -1,10 +1,16 @@
 import { Outlet } from 'react-router-dom';
-import './Movie.css'; // Import the CSS file
+import './Movie.css';
 
 const Movie = () => {
   return (
-    <div className="movie-container">
+    <div className="movie-page">  
       <h1>Movie Page</h1>
+      <div className="lists-container">
+        <div className="table-container">
+          <table className="movie-lists">
+          </table>
+        </div>
+      </div>
       <Outlet />
     </div>
   );
